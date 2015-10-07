16b332d4d2d9de72ca99cd1906caaa0719e96eba
Peter Den Hartog <pddenhar@gmail.com>
2015-10-07 14:01:21 -0500

    Fixed the example

--- example/client.py
+++ example/client.py
@@ -2,7 +2,7 @@
 from postCache import POSTCache
 import json, time
 
-cache = POSTCache("http://dink.com", "cache.db", async_interval=10)
+cache = POSTCache("http://localhost:8080/tracking/api/gps", "cache.db", async_interval=10)
 
 cache.add_request(json.dumps({"date": "now", "latitude":35}))
 time.sleep(20)
